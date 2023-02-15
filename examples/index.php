e709f60e40c426b8c0d954b872e089b04be39b3c
Darragh Blake <darragh@childpaths.ie>
2023-02-15 09:41:56 +0000

    initial push

---
+++ examples/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Darraghb\Bottler\Bottler;
+
+$bottler = new Bottler(
+	['unit' => 'seconds', 
+	'fileName' => __FILE__]);
+
+$bottler->performance('mySlowTest', 20, 5000);
+
+$bottler->performance();
+
+function myTest()
+{
+	$ans = '';
+	for ($i=0; $i < 1000; $i++) { 
+		$ans = $i;
+	}
+
+	return $ans;
+}
+
+
+function mySlowTest($start= false, $loop = false)
+{
+	$ans = '';
+	for ($i=0; $i < $loop; $i++) { 
+		$ans = $i;
+	}
+}
---
+++ src/Bottler.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Darraghb\Bottler;
+
+class Bottler{
+
+	protected $name;
+	protected $unit;
+	protected $fileName;
+
+function __construct($args){
+
+	$this->unit = $args['unit'] ?? 'seconds';
+	$this->fileName = $args['fileName'];
+
+	require_once $this->fileName;
+
+}
+
+public function performance($functionName = null)
+{
+	if(!isset($functionName)){
+		echo "You must specify a function name!";
+		die();
+	}
+	try{
+
+	//remove function name
+	$args = func_get_args();
+	array_shift($args);
+		
+	$start = microtime(true);
+
+	call_user_func_array($functionName, $args);
+
+	$end = microtime(true);
+
+	return $this->calculateTime($end, $start, $functionName);
+
+	}catch(Exception $e){
+		$e->getMessage();
+	}
+	
+}
+
+private function calculateTime($end, $start, $functionName = false)
+{
+
+	switch ($this->unit) {
+		case 'seconds':
+			echo $functionName . ' takes '.(round($end - $start, 5) * 1000).' seconds! <br>'.PHP_EOL;
+			break;
+		case 'nanoseconds':
+			echo $functionName . ' takes '.(round($end - $start, 5) * 10000).' nanoseconds! <br>'.PHP_EOL;
+			break;
+		
+		default:
+			echo $functionName . ' takes '.(round($end - $start, 5) * 1000).' seconds! <br>'.PHP_EOL;
+			break;
+	}
+
+}
+
+}
